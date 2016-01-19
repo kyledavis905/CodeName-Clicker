21bf5bf61fe990c2d93a27378968c785ca3568c4
QuadraG <Gebril.Gabriel@gmail.com>
2016-01-19 12:17:01 -0500

    Created the splash screen. No integration as of yet.

--- src/CodeNameSplash.java
+++ src/CodeNameSplash.java
@@ -1,4 +1,87 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
 
-public class CodeNameSplash {
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JLabel;
+import java.awt.Color;
+import java.awt.Font;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JButton;
 
+public class CodeNameSplash extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3650023916131184587L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					CodeNameSplash frame = new CodeNameSplash();
+					frame.setVisible(true);
+					frame.setLocationRelativeTo(null);  
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public CodeNameSplash() {
+		setTitle("CodeName Clicker");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		
+		JLabel lblCodenameClicker = new JLabel("CodeName Clicker");
+		lblCodenameClicker.setFont(new Font("Tahoma", Font.BOLD, 24));
+		lblCodenameClicker.setForeground(Color.RED);
+		
+		JLabel lblDeveopersGabrielGebril = new JLabel("Deveopers Gabriel Gebril and Kyle Davis");
+		
+		JButton btnStartButton = new JButton("Start");
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addContainerGap(108, Short.MAX_VALUE)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
+							.addComponent(lblCodenameClicker)
+							.addGap(98))
+						.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
+							.addComponent(lblDeveopersGabrielGebril)
+							.addContainerGap())
+						.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
+							.addComponent(btnStartButton, GroupLayout.PREFERRED_SIZE, 153, GroupLayout.PREFERRED_SIZE)
+							.addGap(130))))
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(lblCodenameClicker)
+					.addGap(67)
+					.addComponent(btnStartButton, GroupLayout.PREFERRED_SIZE, 46, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED, 73, Short.MAX_VALUE)
+					.addComponent(lblDeveopersGabrielGebril)
+					.addContainerGap())
+		);
+		contentPane.setLayout(gl_contentPane);
+	}
 }
